efd7097e75b8aadf6742defacd008efe942f78dd
rares vasiliu <rv.vasiliu@gmail.com>
2026-01-05 16:34:47 +0100

    feat: add domain errors

---
+++ src/main/kotlin/io/energyconsumptionoptimizer/mapservice/domain/errors/DomainError.kt
@@ -0,0 +1,22 @@
+package io.energyconsumptionoptimizer.mapservice.domain.errors
+
+abstract class DomainException(
+    message: String,
+    cause: Throwable? = null,
+) : RuntimeException(message, cause)
+
+class ZoneIDNotFoundException(
+    id: String,
+) : DomainException("Zone ID $id not found")
+
+class ZoneNameAlreadyExistsException(
+    name: String,
+) : DomainException("Zone with name '$name' already exists")
+
+class SmartFurnitureHookupIDNotFoundException(
+    id: String,
+) : DomainException("Smart furniture hookup ID $id not found")
+
+class SmartFurnitureHookupAlreadyExistsException(
+    id: String,
+) : DomainException("Smart furniture hookup with ID $id already exists")
